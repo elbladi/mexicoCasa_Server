60e7aab1b2b3392c9e680726b10c09759a14c851
IrvinGalindo <irvin_its@outlook.es>
2020-05-31 14:22:13 -0500

    fix(Token): Add isCustomer

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,14 +3,11 @@ const HttpError = require('../util/http-error');
 
 const authRole = (role) => {
     return (decodedToken, req, res, next) => {
-        console.log(decodedToken)
         if (role !== decodedToken.isCustomer) {
-            console.log(role +" "+ decodedToken.isCustomer)
             return next(new HttpError('Permiso denegado', 401));
         }
         next();
     }
-
 }
 
 module.exports = {

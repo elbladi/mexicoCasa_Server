5b233a7b01c0b1a9570914c5d601ee78d91088cd
elbladi <oirbl@hotmail.com>
2020-06-26 00:39:07 -0500

    add update client data and password

--- controllers/clientController.js
+++ controllers/clientController.js
@@ -3,6 +3,7 @@ const HttpError = require('../util/http-error');
 const instance = require('../firebase');
 require('firebase/firestore');
 const { v4: uuid } = require('uuid');
+const bcrypt = require('bcryptjs');
 
 // const fb = require('firebase/app');
 // const x = fb.initializeApp(firebaseConfig);
@@ -107,7 +108,7 @@ const getLoggedClient = async (req, res, next) => {
                 }
             })
 
-            
+
     } catch (error) {
         console.log(error);
         return next(new HttpError('Algo salio mal. Por favor, intentalo de nuevo', 503));
@@ -115,7 +116,103 @@ const getLoggedClient = async (req, res, next) => {
 
 }
 
+const updateClient = (req, res, next) => {
+    const clientId = req.params.clientId
+    if (!clientId) return next(new HttpError("Cliente no encontrado", 404));
+    let firebase = instance.getInstance();
+
+    try {
+        firebase.firestore().collection('clients').doc(clientId).set({
+            ...req.body
+        }, { merge: true })
+            .then(_ => res.status(200).send())
+            .catch(_ => next(new HttpError("No se pudo actualizar el Cliente", 503)))
+    } catch (error) {
+        return next(new HttpError("Algo salio mal. Por favor, intentalo de nuevo", 404));
+    }
+}
+
+const updatePassword = async (req, res, next) => {
+    const clientId = req.params.clientId
+    if (!clientId) return next(new HttpError("Cliente no encontrado", 404));
+    let firebase = instance.getInstance();
+
+    /**
+     * Verify the user exist
+     * if it does, get the hashed password
+     */
+    let userExist;
+    try {
+        userExist = await firebase.firestore().collection('users').doc(clientId)
+            .get()
+            .then(doc => {
+                if (!doc.exists) return false;
+                return doc.data();
+            })
+    } catch (error) {
+        console.log(error)
+        return next(new HttpError('Algo salio mal. Por favor, intentalo de nuevo', 503));
+    }
+
+    if (!userExist) return next(new HttpError('Algo salio mal. Por favor, intentalo de nuevo', 503));
+
+    /**
+     * compare password provided by user and the one stored
+     */
+    let correctPassword;
+    try {
+        correctPassword = await bcrypt.compare(req.body.current, userExist.password)
+            .then(isEqual => {
+                if (isEqual) return true;
+                else return false;
+            })
+    } catch (error) {
+        return next(new HttpError('Algo salio mal. Por favor, intentalo de nuevo', 503));
+    }
+
+    if (!correctPassword) {
+        return next(new HttpError('Algo salio mal. Por favor, intentalo de nuevo', 503));
+    }
+
+    /**
+     * Hash the provided password
+     */
+
+    let hashedPassword;
+    try {
+        hashedPassword = await bcrypt.hash(req.body.newP, 12)
+            .then(hash => {
+                return hash;
+            })
+            .catch(err => '')
+    } catch (error) {
+        return next(new HttpError('Creacion de usuario fallo', 500));
+    }
+
+    if (!hashedPassword) {
+        return next(new HttpError('Creacion de usuario fallo', 500));
+    }
+
+    /**
+     * update password in users collection
+     */
+
+    try {
+        firebase.firestore().collection('users').doc(clientId).set({
+            password: hashedPassword
+        }, { merge: true })
+            .then(_ => res.status(200).send())
+            .catch(_ => next(new HttpError("No se pudo actualizar el Cliente", 503)))
+    } catch (error) {
+        return next(new HttpError("Algo salio mal. Por favor, intentalo de nuevo", 404));
+    }
+    
+}
+
+
 exports.getBusinesses = getBusinesses;
 exports.getBusiness = getBusiness;
 exports.checkout = checkout;
 exports.getLoggedClient = getLoggedClient;
+exports.updateClient = updateClient;
+exports.updatePassword = updatePassword;
--- routes/clientRoute.js
+++ routes/clientRoute.js
@@ -11,6 +11,8 @@ router.post('/checkout', clientController.checkout);
 router.get('/businesses', clientController.getBusinesses);
 router.get('/getBusiness/:idBusiness', clientController.getBusiness);
 router.get('/getClient/:clientId', clientController.getLoggedClient);
+router.patch('/updateClient/:clientId', clientController.updateClient);
+router.patch('/updatePassword/:clientId', clientController.updatePassword);
 
 
 module.exports = router
